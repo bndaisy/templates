3b0402c61d98d627f69e620409689ab172f28da9
bndaisy <daisy.brkn@gmail.com>
2021-04-24 19:27:07 +0300

    Added filter & sorter

---
+++ hotelcats/src/components/select/select.js
@@ -0,0 +1,5 @@
+export class Select {
+  constructor(settings) {
+
+  }
+}
--- hotelcats/src/js/main.js
+++ hotelcats/src/js/main.js
@@ -1,2 +1,6 @@
 import "./dynamic-navigation-elements";
 import "../components/carousel/carousel";
+
+import { Select } from "../components/select/select";
+
+const select = new Select();
