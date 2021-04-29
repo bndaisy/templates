320efec5b641cf75adc22283af4e2d0c912b7c71
bndaisy <daisy.brkn@gmail.com>
2021-04-29 15:08:05 +0300

    Added register popup

---
+++ vamsvet/src/components/popup/popup.js
@@ -0,0 +1,7 @@
+const showPopUp = (popup) => popup.classList.add('is-active');
+const hidePopUp = (popup) => popup.classList.remove('is-active');
+
+export {
+  showPopUp,
+  hidePopUp,
+};
--- vamsvet/src/js/main.js
+++ vamsvet/src/js/main.js
@@ -1,10 +1,11 @@
 import brgmenu from '../components/burger-menu/burger-menu';
+import { showPopUp, hidePopUp } from '../components/popup/popup';
 
 const header = document.querySelector('.header');
 
 function enableNavigation({ target }) {
-  const navigation = this.querySelector('.nav__wrapper');
-  const toggler = this.querySelector('.burger-menu');
+  const navigation = document.querySelector('.nav__wrapper');
+  const toggler = document.querySelector('.burger-menu');
 
   brgmenu(target, toggler, navigation);
 
@@ -12,4 +13,17 @@ function enableNavigation({ target }) {
   else document.body.style.overflow = 'visible';
 }
 
-header.addEventListener('click', enableNavigation);
+function enablePopUp({ target }) {
+  const popup = document.querySelector('.popup');
+
+  if (target.closest('.header__button')) showPopUp(popup);
+  if (target.classList.contains('popup') || target.closest('.popup__close')) hidePopUp(popup);
+
+  if (popup.classList.contains('is-active')) document.body.style.overflow = 'hidden';
+  else document.body.style.overflow = 'visible';
+}
+
+header.addEventListener('click', function (event) {
+  enableNavigation(event);
+  enablePopUp(event);
+});
