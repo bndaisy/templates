63a72fc4afef34ebd4b3ab5360bb889a77fb1137
bndaisy <daisy.brkn@gmail.com>
2021-05-02 23:56:34 +0300

    Added subscribe component

--- vamsvet/src/js/main.js
+++ vamsvet/src/js/main.js
@@ -1,22 +1,15 @@
+import handler from './handlers';
 import navigation from '../components/burger-menu/burger-menu';
 import popup from '../components/popup/popup';
 
 const page = document.querySelector('.home-page');
 const header = document.querySelector('.header');
+
+const banner = page.querySelector('.banner');
+const subscribe = page.querySelector('.subscribe');
+const subscribeTitle = subscribe.querySelector('[class*=title]');
 const register = document.querySelector('.popup');
-const title = register.querySelector('[class*=title]');
-
-// Function add tag in element after word
-function returnAfter(word, element) {
-  const content = element.textContent.split(' ');
-  element.textContent = '';
-
-  content.forEach((item, index) => {
-    if (item === word) content.splice(index + 1, 0, '<br/>');
-  });
-
-  return content.join(' ');
-}
+const registerTitle = register.querySelector('[class*=title]');
 
 page.addEventListener('click', ({ target }) => {
   if (target.classList.contains('popup') || target.closest('.form__close')) popup.disablePopUp(register);
@@ -27,5 +20,13 @@ header.addEventListener('click', function ({ target }) {
   if (target.closest('.button')) popup.enablePopUp(register);
 });
 
+// Fix the banner textline for coloring
+banner.querySelectorAll('[class*=textline]')
+  .forEach((textline) => {
+    if (textline.textContent.includes('ВамСвет.Дизайн')) textline.insertAdjacentHTML('afterbegin', handler.insertTag(textline, 'ВамСвет.Дизайн', 'span', 'banner__fragment--accent'));
+  });
+
+subscribeTitle.insertAdjacentHTML('afterbegin', handler.insertTag(subscribeTitle, 'ВамСвет.Дизайн', 'span', 'subscribe__fragment--accent'));
+
 // Fix the title for responsive design
-title.insertAdjacentHTML('afterbegin', returnAfter('Регистрация', title));
+registerTitle.insertAdjacentHTML('afterbegin', handler.insertTag(registerTitle, 'Регистрация', 'br'));
